0a9701e179aa0682e1a17687d0840a2d73cd4a80
NotYevvie <yevgen@yev.ai>
2025-03-14 15:11:06 -0500

    zone and kickoff pt2

--- src/cloudflare/index.ts
+++ src/cloudflare/index.ts
@@ -1,3 +1,9 @@
-export const testCloudflareFunction = () => {
-  console.log('Aight, lets get it poppin');
-};
+import * as cloudflare from '@pulumi/cloudflare';
+
+/**
+ * Retrieves a top-level domain (TLD) Cloudflare Zone by name.
+ *
+ * @param name - The name of the Cloudflare Zone to retrieve
+ * @returns A Cloudflare Zone resource
+ */
+export const getRootCloudflareZone = (name: string) => cloudflare.getZone({ name });
